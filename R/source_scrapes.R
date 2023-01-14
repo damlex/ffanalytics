93eb35e87c8e3a8b62143a87881b32d0c0bb594f
damlex <damlex@gmail.com>
2023-01-14 08:06:26 -0500

    log more and Bump Version

--- R/source_scrapes.R
+++ R/source_scrapes.R
@@ -983,8 +983,8 @@ scrape_fantasypros = function(pos = c("QB", "RB", "WR", "TE", "K", "DST"),
   if(is.null(week)) {
     week = get_scrape_week()
   }
-
-  
+    message("*******week***********")
+    message(scrape_week)
 
   if(week > 0) {
     scrape_week = paste0(".php?week=", week)
@@ -992,6 +992,7 @@ scrape_fantasypros = function(pos = c("QB", "RB", "WR", "TE", "K", "DST"),
     scrape_week = ".php?week=draft"
   }
 
+  message("*******scrape_week***********")  
   message(scrape_week)
   
   base_link = paste0("https://www.fantasypros.com/nfl/projections")
